c958a5e69731fda26d99809c79d10cdb3e1408f5
Andrey <Andrey@DESKTOP-T87JMUL>
2025-08-17 13:10:54 +0300

    Добавьте файлы проекта.

---
+++ NovoyaZ/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Novoyaz
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> dict = new Dictionary<string, string>
+        {
+            { "война", "мир" },
+            { "свобода", "рабство" },
+            { "незнание", "сила" },
+            { "большому брату", "бб" },
+            { "большой брат", "бб" },
+            { "большого брата", "бб" },
+            { "большим братом", "бб" },
+            { "большом брате", "бб" },
+            { "Австрали", "Океани" },
+            { "Исланди", "Океани" },
+            { "Америка", "Океания" },
+            { "Америку", "Океанию" },
+            { "Америке", "Океании" },
+            { "Америки", "Океании" },
+            { "Америкой", "Океанией" },
+            { "Африка", "Океания" },
+            { "Африку", "Океанию" },
+            { "Африке", "Океании" },
+            { "Африки", "Океании" },
+            { "рабочие", "пролы" },
+            { "Антарктида", "Океания" },
+            { "пролетариат", "пролы" },
+            { "пропаганда", "пролфид" },
+            { " до ", "анте" },
+            { "иначе", "анте" },
+            { "Великобритания", "Аирстрип Один" },
+            { "Соединенное Королевство", "Аирстрип Один" },
+            { "очень", "даблплюс" },
+            { "невероятно", "даблплюс" },
+            { "одинаково", "эквил" },
+            { "одинаковы", "эквил" },
+            { "полный", "фулвайс" },
+            { "полностью", "фулвайс" },
+            { "правильно", "гудвайс" },
+            { "язык", "новояз" },
+            { "английский", "старояз" }
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Новояз Переводчик";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = textBox1.Text;
+            var sortedDict = dict.OrderByDescending(p => p.Key.Length);
+            string output = input;
+
+            foreach (var pair in sortedDict)
+            {
+                output = Regex.Replace(output, Regex.Escape(pair.Key), m =>
+                {
+                    string tr = pair.Value;
+                    if (char.IsUpper(m.Value[0]))
+                        return char.ToUpper(tr[0]) + tr.Substring(1);
+                    return tr;
+                },
+                RegexOptions.IgnoreCase);
+            }
+
+            textBox2.Text = output;
+        }
+    }
+}
---
+++ NovoyaZ/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("NovoyaZ")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NovoyaZ")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("b26168a2-34ce-4f2f-ae58-3cac44d02276")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
