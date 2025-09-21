d93927fbb88e5f32b4e63d9a00b927990cca7c8f
Andrzej <sjolpypau@gmail.com>
2025-09-21 12:51:45 +0300

    Все массивы (словари и лист для удаления) вынесены в отдельные файлы.

---
+++ NovoyaZ/lists.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovoyaZ
+{
+    public static class lists
+    {
+        public static List<string> zapretki = new List<string>
+        {
+            "бунт",
+            "революция",
+            "восстание",
+            "братство",
+            "равенство",
+            "ненависть",
+            "мысль",
+            "идея",
+            "самостоятельность",
+            "анархия",
+            "право",
+            "справедливость",
+            "эротика",
+            "измена",
+            "счастье",
+            "радость",
+            "печаль",
+            "страх",
+            "тоска",
+            "грусть",
+            "ненужный",
+            "республика",
+            "оппозиция",
+            "заговор",
+            "забастовка",
+            "террор",
+            "критика",
+            "сомнение"
+        };
+    }
+}
